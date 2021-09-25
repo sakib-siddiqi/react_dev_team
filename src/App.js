dfea258bb308275071e7da437842be935dde7888
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-09-25 23:18:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="App-header">
+      <h1 className="App-header">
+        Hi react.js
+      </h1>
+    </div>
+  );
+}
+export default App;
